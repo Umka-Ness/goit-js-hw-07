7b26e67703977f3809e0281dc52a8f5959c539a4
Umka_Ness <melnik.dmitri2012@gmail.com>
2023-01-19 00:53:39 +0100

    update all 2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,8 +3,26 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
-const instance = basicLightbox.create(`
-	<h1>Dynamic Content</h1>
-	<p>You can set the content of the lightbox with JS.</p>
-`)
-instance.show()
+
+
+const item = document.querySelector(".gallery")
+const link = document.querySelector(".gallery__link")
+
+const images = galleryItems.map(i => `<a class="gallery__link" href="${i.original}"><img src = "${i.preview}" data-source="${i.original}" alt="${i.description}" width="" height=""  class="gallery__image"></img></a>`).join("")
+item.insertAdjacentHTML("beforeend",images)
+item.classList.add("gallery__item")
+
+item.addEventListener("click",e => {
+    e.preventDefault()
+    if(e.target === item) {
+        return
+    }
+    
+    const selectedImage = e.target.getAttribute('data-source')
+	let lightbox = new SimpleLightbox('.gallery a', { target});
+	
+    lightbox()
+	
+})
+
+
