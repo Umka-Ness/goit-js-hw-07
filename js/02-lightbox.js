fda12d758b15743758e924437c906f89c2a92156
Umka_Ness <melnik.dmitri2012@gmail.com>
2023-01-19 13:21:11 +0100

    update 2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,21 +8,18 @@ console.log(galleryItems);
 const item = document.querySelector(".gallery")
 const link = document.querySelector(".gallery__link")
 
-const images = galleryItems.map(i => `<a class="gallery__link" href="${i.original}"><img src = "${i.preview}" data-source="${i.original}" alt="${i.description}" width="" height=""  class="gallery__image"></img></a>`).join("")
+const images = galleryItems.map(i => `<a class="gallery__item" href="${i.original}">
+<img class="gallery__image" src="${i.preview}" alt="${i.description}" />
+</a>`).join("")
 item.insertAdjacentHTML("beforeend",images)
-item.classList.add("gallery__item")
 
 item.addEventListener("click",e => {
     e.preventDefault()
     if(e.target === item) {
         return
     }
-    
-    const selectedImage = e.target.getAttribute('data-source')
-	let lightbox = new SimpleLightbox('.gallery a', { target});
-	
-    lightbox()
 	
 })
+new SimpleLightbox('.gallery a', {captionDelay:250,captionPosition:"botton",captionsData:"alt"});
 
 
